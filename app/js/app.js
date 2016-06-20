5f4e287408b575b6f20c9c08f961a0138dbe64e0
Albin Rdz <ealbinu@gmail.com>
2016-06-20 11:32:03 -0500

    DragonWebeloper3 | v0.1
    Alpha… alpha…  zero

---
+++ app/js/app.js
@@ -0,0 +1,22 @@
+var app = angular.module('dw3', [
+  'ng-backstretch',
+  'ngSwipebox',
+  'tooltipster',
+  'duScroll',
+  'slickCarousel'
+]);
+
+app.config(function(){
+
+});
+
+
+app.run(function(){
+
+});
+
+
+app.controller('MainCtrl', function($scope){
+
+
+});
